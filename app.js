fd28613295857632c148eddc56650488b21ce378
lynnjepsen <lynnjepsen@google.com>
2018-10-22 13:57:28 -0700

    made flower form

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-import {MDCRipple} from '@material/ripple/index';
+import {MDCTopAppBar} from '@material/top-app-bar';
+import {MDCChipSet} from '@material/chips';
+import {MDCTextField} from '@material/textfield';
+import {MDCTextFieldIcon} from '@material/textfield/icon';
+import {MDCTextFieldHelperText} from '@material/textfield/helper-text';
+import {MDCRipple} from '@material/ripple';
 
-const ripple = new MDCRipple(document.querySelector('.foo-button'));
+const topAppBarEl = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new MDCTopAppBar(topAppBarEl);
+
+const chipSetEl = document.querySelector('.mdc-chip-set');
+const chipSet = new MDCChipSet(chipSetEl);
+
+const textFieldEl = document.querySelector('.mdc-text-field');
+const textfield = new MDCTextField(textFieldEl);
+
+//const icon = new MDCTextFieldIcon(document.querySelector('.mdc-text-field-icon'));
+const helperText = new MDCTextFieldHelperText(document.querySelector('.mdc-text-field-helper-text'));
+
+const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
