029bd2089ed1987c560af513ee4d618a1dd6535e
lynnjepsen <lynnjepsen@google.com>
2018-10-24 13:58:23 -0700

    attempt to add checkboxes

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 import {MDCTopAppBar} from '@material/top-app-bar';
-import {MDCChipSet} from '@material/chips';
 import {MDCTextField} from '@material/textfield';
 import {MDCTextFieldIcon} from '@material/textfield/icon';
 import {MDCTextFieldHelperText} from '@material/textfield/helper-text';
 import {MDCRipple} from '@material/ripple';
+import {MDCCheckbox} from '@material/checkbox';
 
 const topAppBarEl = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarEl);
-
-const chipSetEl = document.querySelector('.mdc-chip-set');
-const chipSet = new MDCChipSet(chipSetEl);
 
 const textFieldEl = document.querySelector('.mdc-text-field');
 const textfield = new MDCTextField(textFieldEl);
@@ -18,3 +15,9 @@ const textfield = new MDCTextField(textFieldEl);
 const helperText = new MDCTextFieldHelperText(document.querySelector('.mdc-text-field-helper-text'));
 
 const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
+
+const checkboxes = [];
+document.querySelectorAll('.mdc-checkbox').forEach(function(checkboxEl) {
+  const checkbox = new MDCCheckbox(checkboxEl);
+  checkboxes.push(checkbox);
+})
